b5a5f6413ee32415935fa86428c85372214d9ec2
Thomas Barber <thomas.barber@sap.com>
2022-05-16 12:37:00 +0000

    Removing extra files

--- semattack/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmonadfa -lmonabdd -lmonamem -lstranger -lboost_program_options -lboost_regex -lboost_filesystem -lboost_system
-
